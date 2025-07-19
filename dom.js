d674e5804cf321653e935a0b82031c9d6515d070
jeyden94 <jeyden94@gmail.com>
2025-07-19 18:59:20 -0500

    Significant progress on addition and subtraction functionality. Still need to figure out how to add longer sequence of numbers great than 1 digit

--- dom.js
+++ dom.js
@@ -1,13 +1,33 @@
-
 // All Clear Button //
 
 var allClearBtn = document.getElementById("all-clear-btn").addEventListener("click", clearScreen);
 
 function clearScreen() {
     document.getElementById("output-text").textContent = "0";
+    termOne = null;
+    termTwo = null;
+    trackedOperand = null;
 }
 
+// Delete Button //
 
+var deleteBtn = document.getElementById("delete-btn").addEventListener("click", deleteLastNum);
+
+function deleteLastNum() {
+    var output = document.getElementById("output-text");
+    if (output.textContent.length > 1) {
+        output.textContent = output.textContent.slice(0, -1);
+    }
+    else {output.textContent = "0"};
+};
+
+
+// Zero Button //
+
+var zeroBtnValue = document.getElementById("zero-btn").textContent;
+var zeroBtnListener = document.getElementById("zero-btn").addEventListener("click", (e) => {
+    numberBtnClick(e, zeroBtnValue);
+});
 
 // One Button //
 
@@ -72,36 +92,107 @@ var nineBtnListener = document.getElementById("nine-btn").addEventListener("clic
     numberBtnClick(e, nineBtnValue);
 });
 
-
+// Function for Numbered Buttons //
 
 function numberBtnClick(e, btnValue) {
     var currentOutput = document.getElementById("output-text").textContent;
     if (currentOutput === "0") {
         document.getElementById("output-text").textContent = btnValue;
-    } else {
+    } else if (currentOutput.length > 8) { 
+        return null;
+    } else if (termOne !== null & termTwo !== null) {
+        document.getElementById("output-text").textContent = btnValue;
+    }
+    else {
         document.getElementById("output-text").textContent += btnValue;
     }
-    
-}
+};
+
+// --- Operand Buttons --- //
+
+var termOne = null;
+var termTwo = null;
+var trackedOperand = "";
+
+// Addition Button //
+
+var addBtn = document.getElementById("add-btn").textContent;
+
+var addBtnListener = document.getElementById("add-btn").addEventListener("click", (e) => {
+    createAdditionExpression(e, addBtn);
+});
+
+function createAdditionExpression(e, addBtn) {
+    if (termOne === null) { 
+        var currentOutput = document.getElementById("output-text").textContent;
+        termOne = parseInt(currentOutput);
+        document.getElementById("output-text").textContent = "0";
+    }
+    else if (termOne !== null) {
+
+        var currentOutput = document.getElementById("output-text").textContent;
+        termTwo = parseInt(currentOutput);
+
+        if (trackedOperand === addBtn) {
+            sum = addTerms(termOne, termTwo);
+            termOne = sum;
+            document.getElementById("output-text").textContent = `${sum}`;
+        }
+
+        else if (trackedOperand === subtractBtn) {
+            difference = subtractTerms(termOne, termTwo);
+            termOne = difference;
+            document.getElementById("output-text").textContent = `${difference}`;
+        }
+    }
+    trackedOperand = addBtn;
+};
+
+function addTerms(termOne, termTwo) {
+    var sum = termOne + termTwo;
+    console.log("Sum: " + sum);
+    return sum;
+};
 
 
+// Subtract Button //
 
+var subtractBtn = document.getElementById("subtract-btn").textContent;
 
+var subtractBtnListener = document.getElementById("subtract-btn").addEventListener("click", (e) => {
+    createSubtractionExpression(e, subtractBtn);
+});
 
+function createSubtractionExpression(e, subtractBtn) {
+    if (termOne === null) { 
+        var currentOutput = document.getElementById("output-text").textContent;
+        termOne = parseInt(currentOutput);
+        document.getElementById("output-text").textContent = "0";
+        trackedOperand = subtractBtn;
+    }
+    else if (termOne !== null) {
 
+        var currentOutput = document.getElementById("output-text").textContent;
+        termTwo = parseInt(currentOutput);
 
-// const add = function(...nums) {
-// 	return nums.reduce((summedNums, eachNum) => summedNums + eachNum, 0);
-// };
+        if (trackedOperand === addBtn) {
+            sum = addTerms(termOne, termTwo);
+            termOne = sum;
+            document.getElementById("output-text").textContent = `${sum}`;
+        }
 
-// const subtract = function(...nums) {
-// 	return nums.reduce((subtractedNums, eachNum) => subtractedNums - eachNum);
-// };
+        else if (trackedOperand === subtractBtn) {
+            difference = subtractTerms(termOne, termTwo);
+            termOne = difference;
+            document.getElementById("output-text").textContent = `${difference}`;
+        }
+    }
+    trackedOperand = subtractBtn;
+};
 
-// const sum = function(nums) {
-// 	return nums.reduce((summedNums, eachNum) => summedNums + eachNum, 0);
-// };
+function subtractTerms(termOne, termTwo) {
+    var difference = termOne - termTwo;
+    console.log("Difference: " + difference);
+    return difference;
+};
 
-// const multiply = function(nums) {
-//   return nums.reduce((numsProduct, eachNum) => numsProduct * eachNum, 1);
-// };
